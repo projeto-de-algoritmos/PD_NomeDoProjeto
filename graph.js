6a0022f5e0fc12a563edf043f26529bb97f00d7a
Samuel Nogueira Bacelar <samuelnbacelar@hotmail.com>
2022-09-04 10:48:00 -0300

    arrumando variaveis

--- graph.js
+++ graph.js
@@ -11,7 +11,7 @@ export class Disciplina {
         this.nome = disciplina
         let sigla = ""
         const palavras = disciplina.split(" ")
-        for (const i in palavras)
+        for (let i in palavras)
             if(palavras[i].length > 3)
                 sigla += palavras[i][0]
             else if (/^-?\d+$/.test(palavras[i]))
@@ -23,9 +23,9 @@ export class Disciplina {
     }
 
     recalculaPesos(listaDeDisciplinas) {
-        for(i in listaDeDisciplinas) {
+        for(let i in listaDeDisciplinas) {
             const preRequisitoss = listaDeDisciplinas[i].preRequisitos
-            for (const j=0;j<preRequisitoss.length;j++)
+            for (let j=0;j<preRequisitoss.length;j++)
                 if(preRequisitoss[j] == this.nome)
                     this.peso += listaDeDisciplinas[i].peso + 1
         }
@@ -38,14 +38,14 @@ export class Disciplina {
 
 export const listaDisciplina = [];
 const valores = Object.values(dados)
-for(i in valores)
-    listaDisciplina[valores[i].nome] = new Disciplina(valores[i].nome, valores[i].preRequisitos, valores[i].pesoSemestre)
+for(let i in valores)
+    listaDisciplina.push( new Disciplina(valores[i].nome, valores[i].preRequisitos, valores[i].pesoSemestre))
 
-for(i in listaDisciplina)
+for(let i in listaDisciplina)
     listaDisciplina[i].recalculaPesos(listaDisciplina)
 
-    for(i in listaDisciplina)
+    for(let i in listaDisciplina)
     listaDisciplina[i].recalculaPesosSemestre()
 
-for(i in listaDisciplina)
+for(let i in listaDisciplina)
     console.log(listaDisciplina[i].nome + " -> " + listaDisciplina[i].peso)
